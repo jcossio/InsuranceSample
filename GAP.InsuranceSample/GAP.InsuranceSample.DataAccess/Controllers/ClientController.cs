90fe51e132ab4edce53f817af1138e2e469d182d
Jorge Cossio <jcossio@wfscorp.com>
2019-05-07 10:32:14 -0500

    Work on client methods

--- GAP.InsuranceSample/GAP.InsuranceSample.DataAccess/Controllers/ClientController.cs
+++ GAP.InsuranceSample/GAP.InsuranceSample.DataAccess/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GAP.InsuranceSample.DataAccess.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -43,6 +44,67 @@ namespace GAP.InsuranceSample.DataAccess.Controllers
             {
                 // [TODO] Log the exception
                 throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AddContract(ContractDTO Contract)
+        {
+            // Check parameters
+            if (Contract.CoverageMonths == 0)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            // Add Contract
+            using (var db = new InsuranceDBEntities())
+            {
+                using (var dbContextTransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        // Check that client and policy exists
+                        if (!db.Policy.Any(p => p.PolicyId == Contract.PolicyId && p.Deleted == false))
+                            throw new HttpResponseException(HttpStatusCode.BadRequest);
+                        if (!db.Client.Any(c => c.ClientId == Contract.ClientId))
+                            throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+                        var policy = db.Policy.Where(p => p.PolicyId == Contract.PolicyId && p.Deleted == false).FirstOrDefault();
+                        var newContract = new ClientContract()
+                        {
+                            ClientId = Contract.ClientId,
+                            PolicyId = Contract.PolicyId,
+                            EffectDate = Contract.EffectDate,
+                            CoverageMonths = Contract.CoverageMonths,
+                            MonthlyPremium = policy.MonthlyPremium,
+                            RiskTypeId = policy.RiskTypeId,
+                            Cancelled = false
+                        };
+                        db.ClientContract.Add(newContract);
+                        db.SaveChanges();
+
+                        var covers = db.PolicyCover.Where(c => c.PolicyId == Contract.PolicyId);
+                        // Add coverage
+                        foreach (var cover in covers)
+                        {
+                            db.ClientContractCover.Add(new ClientContractCover()
+                            {
+                                ClientContractCoverId = newContract.ClientContractId,
+                                CoverId = cover.CoverId,
+                                Percentage = cover.Percentage
+                            });
+                        }
+
+                        db.SaveChanges();
+                        dbContextTransaction.Commit();
+                        // Return indication it has been created
+                        return new HttpResponseMessage(HttpStatusCode.Created);
+                    }
+                    catch (Exception)
+                    {
+                        dbContextTransaction.Rollback();
+                        // [TODO] Log the exception
+                        throw new HttpResponseException(HttpStatusCode.InternalServerError);
+                    }
+                }
             }
         }
     }
---
+++ GAP.InsuranceSample/GAP.InsuranceSample.DataAccess/Model/ContractDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GAP.InsuranceSample.DataAccess.Model
+{
+    public class ContractDTO
+    {
+        public int ClientId { get; set; }
+        public int PolicyId { get; set; }
+        public DateTime EffectDate { get; set; }
+        public short CoverageMonths { get; set; }
+    }
+}
