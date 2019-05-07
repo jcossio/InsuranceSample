95815d8dab4da7420958bc680f63333f763000f8
Jorge Cossio <jcossio@wfscorp.com>
2019-05-07 07:45:08 -0500

    Add Policy method

--- GAP.InsuranceSample/GAP.InsuranceSample.DataAccess/Controllers/PolicyController.cs
+++ GAP.InsuranceSample/GAP.InsuranceSample.DataAccess/Controllers/PolicyController.cs
@@ -134,5 +134,71 @@ namespace GAP.InsuranceSample.DataAccess.Controllers
                 throw new HttpResponseException(HttpStatusCode.InternalServerError);
             }
         }
+
+        /// <summary>
+        /// Add a new policy.
+        /// </summary>
+        /// <param name="policy">Policy DTO with covers</param>
+        /// <returns>The created policy DTO</returns>
+        [HttpPost]
+        public PolicyDTO Add(PolicyDTO policy)
+        {
+            // Check parameters
+            // Required Name and at least one cover
+            if (string.IsNullOrEmpty(policy.Name) || policy.Covers == null || policy.Covers.Count == 0)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            // [TODO] Move this to a BL layer
+            // Check business logic
+            // No cover higher than 50% on high risk
+            if (policy.RiskTypeId == 4)
+            {
+                if (policy.Covers.Any(c => c.Percentage > 50))
+                    throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // Add policy
+            using (var db = new InsuranceDBEntities())
+            {
+                using (var dbContextTransaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var newPolicy = new Policy()
+                        {
+                            Name = policy.Name,
+                            Description = policy.Description,
+                            MonthlyPremium = policy.MonthlyPremium,
+                            RiskTypeId = policy.RiskTypeId,
+                            Deleted = false
+                        };
+                        db.Policy.Add(newPolicy);
+                        db.SaveChanges();
+
+                        // Add covers
+                        foreach (var cover in policy.Covers)
+                        {
+                            db.PolicyCover.Add(new PolicyCover()
+                            {
+                                PolicyId = newPolicy.PolicyId,
+                                CoverId = cover.CoverId,
+                                Percentage = cover.Percentage
+                            });
+                        }
+
+                        db.SaveChanges();
+                        dbContextTransaction.Commit();
+                        // Return policy with newly generated Id
+                        return Get(newPolicy.PolicyId);
+                    }
+                    catch (Exception)
+                    {
+                        dbContextTransaction.Rollback();
+                        // [TODO] Log the exception
+                        throw new HttpResponseException(HttpStatusCode.InternalServerError);
+                    }
+                }
+            }
+        }
     }
 }
